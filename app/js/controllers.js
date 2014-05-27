8d4d30de1c6cb3a827e139df99ed38578cdb69ba
francis dufféal <francis.duffeal.external@watchever.com>
2014-05-27 17:22:56 +0200

    product

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,6 +15,36 @@ function getKeys(obj) {
 	return keys;
 }
 
+function signParams(myParams){
+	var keys = getKeys(myParams);
+	var len = keys.length;
+	keys.sort();
+
+	var formatedParams = [];
+	var buffer = '';
+	for (var i = 0; i < len; i++) {
+		var k = keys[i];
+		var temp = encodeURIComponent(myParams[k]).replace(/'/g, '%27');
+		temp = temp.replace(/\(/g, '%28');
+		temp = temp.replace(/\)/g, '%29');
+		if (i > 0) {
+			buffer += '&';
+		}
+		buffer += k + '=' + temp;
+		if (i + 1 == len) {
+			buffer += '07ae9a3a7b6590f3295c792651a0eb7d274c5709';
+		}
+		formatedParams[k] = myParams[k];
+	}
+
+	console.log(buffer);
+	formatedParams.apitv_signature = '' + CryptoJS.MD5(buffer);
+
+	console.log(formatedParams);
+
+	return formatedParams;
+}
+
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope','$http', function($scope, $http) {
 		var myParams = {};
@@ -28,33 +58,7 @@ angular.module('myApp.controllers', [])
 		myParams.level_info = '2';
 		myParams.with_count = 'YES';
 
-		var keys = getKeys(myParams);
-		var len = keys.length;
-		keys.sort();
-
-		var formatedParams = [];
-		var buffer = '';
-		for (var i = 0; i < len; i++) {
-			var k = keys[i];
-			var temp = encodeURIComponent(myParams[k]).replace(/'/g, '%27');
-			temp = temp.replace(/\(/g, '%28');
-			temp = temp.replace(/\)/g, '%29');
-			if (i > 0) {
-				buffer += '&';
-			}
-			buffer += k + '=' + temp;
-			if (i + 1 == len) {
-				buffer += '07ae9a3a7b6590f3295c792651a0eb7d274c5709';
-			}
-			formatedParams[k] = myParams[k];
-		}
-
-		console.log(buffer);
-		formatedParams.apitv_signature = '' + CryptoJS.MD5(buffer);
-
-		console.log(formatedParams);
-
-
+		var formatedParams = signParams(myParams);
 
 		$http({
 			method: 'POST',
@@ -74,4 +78,36 @@ angular.module('myApp.controllers', [])
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+  }])
+	.controller('ProductCtrl',['$scope','$routeParams','$http',function($scope,$routeParams,$http){
+		$scope.folderId = $routeParams.folderId;
+
+		var myParams = {};
+
+		myParams.login = 'gui@vme.com';
+		myParams.password = '234561';
+		myParams.apitv_client_id = 'cb57c714a3bd14d4b883126815f78a63801cbefcDE';
+		myParams.apitv_unique_device_id = '1234';
+		myParams.code_lang = 'de_DE';
+		myParams.folder_id = $scope.folderId;
+		myParams.level_info = '2';
+		myParams.with_count = 'YES';
+
+		var formatedParams = signParams(myParams);
+
+		$http({
+			method: 'POST',
+			url: 'http://apitv.watchever.com/ps3/v1.0/getDetails.json',
+			params: formatedParams
+		}).success(function(data, status, headers, config) {
+			// this callback will be called asynchronously
+			// when the response is available
+			console.log(data);
+			$scope.product = data[0];
+		}).error(function(data, status, headers, config) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+		});
+
+	}])
+;
